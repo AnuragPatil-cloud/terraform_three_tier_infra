cd838482430bb1695009f86cdf1e6700435dda33
Anurag Patil <patilanurag1703@gmail.com>
2026-04-22 02:54:20 +0530

    Add files via upload

---
+++ three-tier-infra-main/main.tf
@@ -0,0 +1,20 @@
+provider "aws" {
+    region = "ap-south-1"
+}
+
+module "rds" {
+    source = "./modules/rds"
+}
+
+module "eks" {
+    source = "./modules/eks"
+    project = "three-tier-infra"
+    desired_nodes = 2
+    max_nodes  = 2
+    min_nodes  = 2
+    node_instance_type = "t3.medium"
+}
+
+module "s3" {
+    source = "./modules/s3"
+}
---
+++ three-tier-infra-main/modules/s3/main.tf
@@ -0,0 +1,45 @@
+# Create an S3 bucket
+resource "aws_s3_bucket" "arp_bucket" {
+  bucket = "arp-frontend-project-bux"
+}
+
+resource "aws_s3_bucket_website_configuration" "example" {
+  bucket = aws_s3_bucket.arp_bucket.id
+
+  index_document {
+    suffix = "index.html"
+  }
+}
+
+# Disable Block Public Access
+resource "aws_s3_bucket_public_access_block" "example" {
+  bucket = aws_s3_bucket.arp_bucket.bucket
+
+  block_public_acls       = false
+  block_public_policy     = false
+  ignore_public_acls      = false
+  restrict_public_buckets = false
+}
+# Set the bucket policy to allow public read access (use cautiously)
+resource "aws_s3_bucket_policy" "static_website_policy" {
+  bucket = aws_s3_bucket.arp_bucket.id
+
+  policy = jsonencode({
+    Version = "2012-10-17"
+    Statement = [
+      {
+        Effect    = "Allow"
+        Principal = "*"
+        Action    = "s3:GetObject"
+        Resource  = "${aws_s3_bucket.arp_bucket.arn}/*"
+      }
+    ]
+  })
+  depends_on = [aws_s3_bucket_public_access_block.example]
+}
+
+# Output the bucket's website endpoint
+output "website_endpoint" {
+  value = aws_s3_bucket_website_configuration.example.website_endpoint
+   description = "The URL to access the static website"
+}
